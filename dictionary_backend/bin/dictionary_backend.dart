074a3e4aa750aade63ea0ba215eee2ec2d3f6300
Higor Madeira <higorh.11@hotmail.com>
2024-05-22 00:48:06 -0300

    adding back end package

---
+++ dictionary_backend/bin/dictionary_backend.dart
@@ -0,0 +1,5 @@
+import 'package:dictionary_backend/dictionary_backend.dart' as dictionary_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dictionary_backend.calculate()}!');
+}
