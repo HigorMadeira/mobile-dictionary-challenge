63720e55055d88b6be90c95ef80918dae8fbc7de
Higor Madeira <higorh.11@hotmail.com>
2024-05-26 06:22:23 -0300

    Trying voice

--- dictionary_backend/lib/words/words_datasource.dart
+++ dictionary_backend/lib/words/words_datasource.dart
@@ -15,6 +15,7 @@ class WordsDataSource implements IWordsDatasource {
         List<dynamic> jsonResponse = json.decode(response.body);
         final result =
             jsonResponse.map((data) => WordData.fromJson(data)).toList().first;
+        speak(result.word);
         return result;
       } else {
         print('Failed to load word data: ${response.statusCode}');
@@ -23,6 +24,33 @@ class WordsDataSource implements IWordsDatasource {
     } catch (e) {
       print('Error occurred: $e');
       throw Exception('Failed to load word data');
+    }
+  }
+
+  Future<void> speak(String text) async {
+    const String apiKey = 'RtAn0gEL';
+
+    final url = Uri.parse('https://api.responsivevoice.org/v1/text:speak');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $apiKey',
+    };
+    final body = jsonEncode({
+      'text': text,
+      'voice': 'US English Female',
+      'rate': 1.0,
+    });
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+
+      if (response.statusCode == 200) {
+        print('Speech synthesis successful');
+      } else {
+        print('Failed to synthesize speech: ${response.body}');
+      }
+    } catch (e) {
+      print('Error occurred: $e');
     }
   }
 }
